f254c8766c4f2af3a4d75361b165aa647a639453
HyeonGi Min <42742624+HyeonGiMin@users.noreply.github.com>
2024-11-01 15:52:31 +0900

    delete error file

--- src/app/index/page.tsx
+++
@@ -1,8 +0,0 @@
-// pages/index.tsx
-export default function Home() {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
-      </div>
-    );
-  }
